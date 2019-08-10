abf02d8fbce0c5ae5bd1fe8f8a56e2dd966bde7d
mrcool4000 <raghav.m2014@gmail.com>
2019-08-10 09:54:56 -0700

    Attempting to convert to JSONStore

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var fs = require("fs");
+var getJSON = require('get-json')
 
 app.use(express.static(__dirname + "/public"));
 
