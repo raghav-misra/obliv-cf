0d6c057b367e8f8d4e2ef53a9657cd01520a2c5d
mrcool4000 <raghav.m2014@gmail.com>
2019-08-10 18:10:32 -0700

    Revert All JSONStore changes, go back to local json hosting.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-require('dotenv').config()
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var fs = require("fs");
-var getJSON = require('get-json')
 
 app.use(express.static(__dirname + "/public"));
 
